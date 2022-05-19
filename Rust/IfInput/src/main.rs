10b0fe296189ee90ba98f9019867f04617499630
Xanthus <Xanthus58@protonmail.com>
2022-05-20 05:09:54 +0930

    Slight ifinput tweaks

--- Rust/IfInput/src/main.rs
+++ Rust/IfInput/src/main.rs
@@ -1,22 +1,36 @@
 use std::{thread, time::Duration};
 
 fn main(){
+    loop{
+    print!("{esc}c", esc = 27 as char);
     let mut line = String::new();
-
+    
     println!("Will you help the man? (Y/N)");
-
+    
     std::io::stdin().read_line(&mut line).unwrap();
-
-    let y = "Y";
+    
+    let Y = "Y";
+    let y = "y";
+    let N = "N";
+    let n = "n";
+    
     let line = line.trim();
     print!("{esc}c", esc = 27 as char);
     
-    if line == y{
+    
+    if line == y || line == Y{
         println!("The man gives you a peice of gold and thanks you.");
-        thread::sleep(Duration::from_secs(5))
+        thread::sleep(Duration::from_secs(5));
+        break
     } 
-    else{
+    else if line == n || line == N{
         println!("You walk past the man and ignore him");
-        thread::sleep(Duration::from_secs(5))
+        thread::sleep(Duration::from_secs(5));
+        break
     }
+    else{
+        println!("Invalid input, please respond with 'Y' or 'N'");
+        thread::sleep(Duration::from_secs(5));
+    }
+    }  
  }
