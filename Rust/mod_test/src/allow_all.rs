3c1a95dd66c30149ec07122a63d92b98670691b8
Xanthus <Xanthus58@protonmail.com>
2022-06-14 13:44:51 +0930

    added module tests

---
+++ Rust/mod_test/src/allow_all.rs
@@ -0,0 +1,6 @@
+pub fn allow_all_code(){
+    #![allow(unused_variables)]
+    #![allow(dead_code)]
+    #![allow(non_camel_case_types)]
+    #![allow(unused_assignments)]
+}
---
+++ Rust/mod_test/src/main.rs
@@ -0,0 +1,9 @@
+use allow_all::allow_all_code;
+use credits::credits;
+mod credits;
+mod allow_all;
+
+fn main() {
+    credits();
+    allow_all_code();
+}
