5ef8328b8558ab2b6a6d350b3b0e2af7a6ede39f
Xanthus <Xanthus58@protonmail.com>
2022-07-31 16:30:46 +0930

    Add math to DLL test

--- Rust/dlls/src/lib.rs
+++ Rust/dlls/src/lib.rs
@@ -1,13 +1,13 @@
 #[no_mangle]
-pub extern fn lib_test() {
-    println!("Hello, world!");
-    println!("-All code above me is exextued via the dll-\n");
-}
-
-pub fn math(num1: f32, num2: f32) {
+pub fn math(num1: f32, num2: f32) -> String {
     println!("\n-Math-");
-    println!("{} + {} = {}", num1, num2, num1 + num2);
-    println!("{} - {} = {}", num1, num2, num1 - num2);
-    println!("{} * {} = {}", num1, num2, num1 * num2);
-    println!("{} / {} = {}", num1, num2, num1 / num2);
+    let add = num1 + num2;
+    let sub = num1 - num2;
+    let mul = num1 * num2;
+    let div = num1 / num2;
+    let modulo = num1 % num2;
+    return format!(
+        "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}\n{} % {} = {}",
+        num1, num2, add, num1, num2, sub, num1, num2, mul, num1, num2, div, num1, num2, modulo
+    );
 }
--- Rust/dlls/src/main.rs
+++ Rust/dlls/src/main.rs
@@ -1,21 +1,36 @@
-#![crate_type = "dylib"]
+#![crate_type = "cdylib"]
 
 mod lib;
 use crate::lib::*;
 use std::io;
 
 fn main() {
-    lib_test();
+    //clear screen
+    print!("\x1B[2J\x1B[1;1H");
+    
+    println!("Hello, Welcome to a simple DLL library test.");
+    println!("Included in the DLL is a simple program that calculates all the different math variations of 2 numbers.");
+    println!("While the program handles gathering user inputs and displaying the results to the screen.\n");
+    
     //get user input as a number
     println!("Enter a number: ");
     let mut num1 = String::new();
-    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Failed to read line");
     let num1: f32 = num1.trim().parse().expect("Please type a number!");
+
     println!("Enter another number: ");
     let mut num2 = String::new();
-    io::stdin().read_line(&mut num2).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Failed to read line");
     let num2: f32 = num2.trim().parse().expect("Please type a number!");
-    math(num1, num2);
+
+    let math = math(num1, num2);
+    println!("{}", math);
+
+    println!("\nPress enter to exit.");
     let mut line = String::new();
     let _b1 = std::io::stdin().read_line(&mut line).unwrap();
 }
