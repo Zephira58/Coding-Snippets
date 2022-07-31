c6629a2c433c6972513342e1a217503c7c1d8746
Xanthus <Xanthus58@protonmail.com>
2022-07-31 16:08:53 +0930

    Add math as library test

--- Rust/dlls/src/lib.rs
+++ Rust/dlls/src/lib.rs
@@ -3,3 +3,11 @@ pub extern fn lib_test() {
     println!("Hello, world!");
     println!("-All code above me is exextued via the dll-\n");
 }
+
+pub fn math(num1: f32, num2: f32) {
+    println!("\n-Math-");
+    println!("{} + {} = {}", num1, num2, num1 + num2);
+    println!("{} - {} = {}", num1, num2, num1 - num2);
+    println!("{} * {} = {}", num1, num2, num1 * num2);
+    println!("{} / {} = {}", num1, num2, num1 / num2);
+}
--- Rust/dlls/src/main.rs
+++ Rust/dlls/src/main.rs
@@ -1,4 +1,4 @@
-#![crate_type = "cdylib"]
+#![crate_type = "dylib"]
 
 mod lib;
 use crate::lib::*;
@@ -6,8 +6,16 @@ use std::io;
 
 fn main() {
     lib_test();
-    //get user input
-    let mut input = String::new();
-    println!("Press enter to close");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    //get user input as a number
+    println!("Enter a number: ");
+    let mut num1 = String::new();
+    io::stdin().read_line(&mut num1).expect("Failed to read line");
+    let num1: f32 = num1.trim().parse().expect("Please type a number!");
+    println!("Enter another number: ");
+    let mut num2 = String::new();
+    io::stdin().read_line(&mut num2).expect("Failed to read line");
+    let num2: f32 = num2.trim().parse().expect("Please type a number!");
+    math(num1, num2);
+    let mut line = String::new();
+    let _b1 = std::io::stdin().read_line(&mut line).unwrap();
 }
