4af15c6c70de40d8f4195a1659c159dc8bd47a3b
Xanthus <Xanthus58@protonmail.com>
2022-08-20 20:36:30 +0930

    Fix: Made it so that the terminal no longer renders

--- Rust/egui/src/main.rs
+++ Rust/egui/src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 use eframe::egui; //Imports the rendering engine
 use eframe::egui::Visuals; //Imports dark mode
 
