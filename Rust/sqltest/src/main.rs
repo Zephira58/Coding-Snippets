26454bf5f3aa2c0a4d5ed923d50014bb194cff2b
Aviion Gibbs <xanthus58@protonmail.com>
2023-09-12 20:04:26 +0930

    sqltest credits
    added basic accreditation to the sqltest archive.

--- Rust/sqltest/src/main.rs
+++ Rust/sqltest/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-}
+    /* 
+    Title: sqltest
+    Description: A small application to test sql integration with the SQLX library
+    Author: Xanthus58
+    Repo: https://github.com/Xanthus58/Coding-Snippets/tree/main/Rust/sqltest
+    */
+    
