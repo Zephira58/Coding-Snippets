96a71243e66f29d5bcc2237e7f50761fd24a87dc
Aviion Gibbs <xanthus58@protonmail.com>
2024-01-17 18:47:17 +1030

    Add: New randomint api

--- Rust/test-api/src/main.rs
+++ Rust/test-api/src/main.rs
@@ -1,14 +1,30 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use rand::Rng;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/gae")]
+async fn gae() -> impl Responder {
+        HttpResponse::Ok().body("Your very gae!")
+}
+
+#[get("/randomint")]
+async fn randomint() -> impl Responder {
+    let int = rand::thread_rng().gen_range(0..100);
+    println!("Requested number: {}", int.to_string());
+    HttpResponse::Ok().body(int.to_string())
+
+}
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
+
+
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -20,6 +36,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(gae)
+            .service(randomint)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
