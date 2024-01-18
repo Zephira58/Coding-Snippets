fbe2ae2c7d429be366a02be89d8ae9b31d3bcfb5
Aviion Gibbs <xanthus58@protonmail.com>
2024-01-18 18:25:28 +1030

    Remove: Old WIP code

--- Rust/sqltest/src/main.rs
+++
@@ -1,7 +0,0 @@
-    /* 
-    Title: sqltest
-    Description: A small application to test sql integration with the SQLX library
-    Author: Xanthus58
-    Repo: https://github.com/Xanthus58/Coding-Snippets/tree/main/Rust/sqltest
-    */
-    
